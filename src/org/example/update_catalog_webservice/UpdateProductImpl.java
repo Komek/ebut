7bb39005af39ca75c7c5aa085ea16830b13b0191
Stephen Regenscheit <regenscheitstephen@gmail.com>
2014-07-01 14:51:33 +0200

    impl klasse erstellt

---
+++ src/org/example/update_catalog_webservice/UpdateProductImpl.java
@@ -0,0 +1,12 @@
+package org.example.update_catalog_webservice;
+
+public class UpdateProductImpl implements UpdateProductInterface {
+
+	@Override
+	public UpdateResponseType updateCatalog(UpdateRequestType updateRequestPart)
+			throws AuthenticationFaultMessage {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
